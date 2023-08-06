e002040b1fc7c24e5e0403e2d13b7903d5a838f3
Monzurul Islam <Xatta-Trone@users.noreply.github.com>
2023-08-06 16:42:51 +0900

    update table

--- resources/views/table.blade.php
+++ resources/views/table.blade.php
@@ -1,4 +1,21 @@
-<table>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+
+<style>
+        table, th, td {
+  border: 1px solid black;
+  border-collapse: collapse;
+}
+</style>
+</head>
+<body>
+
+    <table>
     <thead>
         <tr>
             <th>Time</th>
@@ -42,3 +59,8 @@
         @endif
     </tbody>
 </table>
+
+
+
+</body>
+</html>
