2a68900951cf2c53eac8e626d6009a110b1512c6
Monzurul Islam <Xatta-Trone@users.noreply.github.com>
2023-08-06 16:32:54 +0900

    download data

--- resources/views/project.blade.php
+++ resources/views/project.blade.php
@@ -1,5 +1,6 @@
 <x-app-layout-component>
     <h1>{{$project->title}}</h1>
+    <a href="{{route('project.export',$project->id)}}"  class="btn btn-primary">Download excel</a>
     <x-alert-component/>
     <div class="my-5">
         <livewire:project-component :project="$project" />
