ac8506309ec33016bfaaa170e41d6ed562eb5bd8
Monzurul Islam <monzurul.ce.buet@gmail.com>
2023-09-27 06:45:52 +0900

    dropbox fix

--- app/Providers/DropboxServiceProvider.php
+++ app/Providers/DropboxServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Spatie\Dropbox\Client;
 use League\Flysystem\Filesystem;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\ServiceProvider;
 use Spatie\FlysystemDropbox\DropboxAdapter;
@@ -24,8 +26,18 @@ class DropboxServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Storage::extend('dropbox', function ($app, $config) {
 
+        // $authorization_token = Cache::remember('dropbox.authorization_token', 14400 - 1, function () {
+        //     return $this->getAuthorizationCode();
+        // });
+
+        $authorization_token =
+            $this->getAuthorizationCode();
+
+        // config(['filesystems.disks.dropbox.authorization_token' => $authorization_token]);
+
+
+        Storage::extend('dropbox', function ($app, $config) use ($authorization_token) {
             $client = new Client(config('filesystems.disks.dropbox.authorization_token'));
             $adapter = new DropboxAdapter($client);
 
@@ -33,5 +45,28 @@ class DropboxServiceProvider extends ServiceProvider
 
             return new FilesystemAdapter($filesystem, $adapter);
         });
+    }
+
+    private function getAuthorizationCode()
+    {
+        // $response = Http::asForm()->post('https://api.dropbox.com/oauth2/token', [
+        //     'grant_type' => 'refresh_token',
+        //     'client_id' => env('DROPBOX_APP_KEY'),
+        //     'client_secret' => env('DROPBOX_APP_SECRET'),
+        //     'refresh_token' => env('DROPBOX_APP_REFRESH_TOKEN'),
+        // ]);
+
+        $client_id  = env('DROPBOX_APP_KEY');
+        $client_secret  = env('DROPBOX_APP_SECRET');
+
+        $response = Http::asForm()->post('https://' . $client_id . ':' . $client_secret . '@api.dropbox.com/oauth2/token', [
+            'grant_type' => 'refresh_token',
+            // 'client_id' => env('DROPBOX_APP_KEY'),
+            // 'client_secret' => env('DROPBOX_APP_SECRET'),
+            'refresh_token' => env('DROPBOX_APP_REFRESH_TOKEN'),
+        ]);
+
+
+        return $response->json()['access_token'];
     }
 }
