dd4a30a7f836b823303b60263c8f60167461da18
Monzurul Islam <monzurul.ce.buet@gmail.com>
2023-10-03 06:10:36 +0900

    added parent project

---
+++ database/migrations/2023_10_03_025712_add_parent_project_id_to_projects_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('projects', function (Blueprint $table) {
+            $table->foreignId('parent_project_id')->nullable()->constrained()->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('projects', function (Blueprint $table) {
+            $table->dropForeign('parent_project_id');
+            $table->dropColumn('parent_project_id');
+        });
+    }
+};
--- resources/js/Layouts/AdminLayout.vue
+++ resources/js/Layouts/AdminLayout.vue
@@ -5,7 +5,7 @@
             class="absolute left-0 top-0 z-9999 flex h-screen w-64 flex-col overflow-y-hidden bg-slate-800 duration-300 ease-linear lg:static lg:translate-x-0 -translate-x-full">
             <!-- logo -->
             <div class="flex items-center justify-between gap-2 px-6 py-5 lg:py-6">
-                <Link href="{{ route('index') }}" class="text-white text-xl font-bold inline-block">
+                <Link href="/" class="text-white text-xl font-bold inline-block">
                     <img :src="img" alt="Logo" class="h-8 inline-block mr-2" /> Route 64DC Ltd.
                 </Link>
             </div>
